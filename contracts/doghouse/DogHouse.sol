144c1c06d7956ba090ee654442eab5a2e463cd84
Shiht Tzu <projectshihtzu@gmail.com>
2021-05-01 16:57:20 -0500

    WIP

--- contracts/doghouse/DogHouse.sol
+++ contracts/doghouse/DogHouse.sol
@@ -19,4 +19,6 @@ contract DogHouse is Ownable {
     }
 
 
+
+
 }
--- contracts/poop/PoopToken.sol
+++ contracts/poop/PoopToken.sol
@@ -1,9 +1,17 @@
 pragma solidity ^0.6.12;
 
 import "./ERC20Untransferable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract PoopToken is ERC20Untransferable {
+contract PoopToken is ERC20Untransferable, Ownable {
     constructor() ERC20Untransferable("Poop Token", "POOP") public {}
 
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) public onlyOwner {
+        _burn(from, amount);
+    }
 
 }
